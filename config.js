b95851f2b49bfd60af35f25488db7f63ed84c669
Pablo Corso <pabcorso@gmail.com>
2020-08-20 18:31:03 +0200

    Regenerate and remove bot token from config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
   prefix: '!',
-  token: 'NzQzOTQzNzIxMDgyNTUyMzQx.XzcBlA.flUxqU0490HfKcFa5PJYrRJT9dc',
+  token: '',
   bnStorePath: path.join(__dirname, 'battle-notifier', 'bn.store.json'),
 };
